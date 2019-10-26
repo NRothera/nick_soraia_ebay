387ec029955be324ce3975a6aa1307ae582d4235
Nicolas Rothera <nickrothera@gmail.com>
2019-10-26 13:47:36 +0100

    removed port binding

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ storeResults.write_to_results(filtered_result);
 
 
 
-app.listen(8989);
-console.log("Now listening on port 8989")
+// app.listen(8989);
+// console.log("Now listening on port 8989")
